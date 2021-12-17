396f78340bd59e147f9e5bb514621372e6615fef
Jan Verhoeven <jan269@live.de>
2021-12-17 13:04:17 +0000

    added setup-script for vms

---
+++ setup-vm.sh
@@ -0,0 +1,27 @@
+#cd /jan/home
+#git clone https://janverh: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@github.com/janverh/kafka-streams-analyse.git
+git config --global user.name "Jan Verhoeven"
+git config --global user.email "jan269@live.de"
+
+sudo apt update
+sudo apt upgrade
+
+sudo apt-get install \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt update
+sudo apt install docker-ce docker-ce-cli containerd.io
+
+
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
+
+wget -P ./jmx-exporter https://repo1.maven.org/maven2/io/prometheus/jmx/jmx_prometheus_javaagent/0.16.1/jmx_prometheus_javaagent-0.16.1.jar
+chmod +x ./jmx-exporter/*.jar
